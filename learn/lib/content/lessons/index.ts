513976e279c39f5143779b35f9afc6da95ae98a4
eksucampusmarketplace-cell <eksucampusmarketplace@gmail.com>
2026-05-28 14:31:42 -0700

    Delete learn directory

--- learn/lib/content/lessons/index.ts
+++
@@ -1,5 +0,0 @@
-export { pythonBasicsLesson } from './python-basics';
-export { jsBasicsLesson } from './js-basics';
-export { whatIsProgrammingLesson } from './what-is-programming';
-
-// Additional lessons will be imported here as they are created
